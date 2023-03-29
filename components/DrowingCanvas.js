8381dbc22eabf1f182d99f2cd94d85606bd206a6
Wesley G. Obi <42078311+W3sley0Bi@users.noreply.github.com>
2023-03-29 12:03:14 +0200

    WIP sign

--- components/DrowingCanvas.js
+++ components/DrowingCanvas.js
@@ -14,7 +14,6 @@ const DrawingCanvas = () => {
       {/* <canvas style={{backgroundColor: 'white'}} onMouseDown={startDrawing} ontouchstart={startDrawing} onMouseUp={finishDrawing} onTouchEnd={finishDrawing} onMouseMove={draw} ref={canvasRef}/> */}
 
       <Modal
-        scroll
         fullScreen
         closeButton
         aria-labelledby="modal-title"
