b47f341987c6c9d677ea89a25b2f9668bf50d09e
Wesley G. Obi <42078311+W3sley0Bi@users.noreply.github.com>
2023-04-03 20:28:18 +0200

    test

--- pages/userFolder/[Uid]/[FolderContent]/Form.js
+++ pages/userFolder/[Uid]/[FolderContent]/Form.js
@@ -38,7 +38,7 @@ export default function Form() {
       return false;
     }
 
-    if (files.length >= 3) return true;
+    if (files.length >= 1) return true;
     alert("you need to fill the Installateur section with min. 3 images!");
     return false;
   };
