5ae4a8f0971e8c252c82fef6506fe0c3197fb157
Wesley G. Obi <42078311+W3sley0Bi@users.noreply.github.com>
2023-04-03 13:22:07 +0200

    wip mail

--- pages/userFolder/[Uid]/[FolderContent]/Form.js
+++ pages/userFolder/[Uid]/[FolderContent]/Form.js
@@ -4,6 +4,8 @@ import FormInstallLK from "../../../../components/FormInstallLK"
 import { useState, useEffect, useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useRouter } from 'next/router'
+import FormData from 'form-data'
+import { useDropzone } from 'react-dropzone';
 import axios from 'axios'
 
 
@@ -12,21 +14,65 @@ export default function Form() {
     const { theme } = useTheme();
     const { Uid,FolderContent } = router.query
     const [pdfData, setPdfData] = useState()
+    const [textArea, setTextArea] = useState()
+    const [files, setFiles] = useState([])
     const token = useSelector((state) => state.token.value);
     const uid = useSelector((state) => state.uid.value);
     const role = useSelector((state) => state.role.value);
+
 
 const callback = props => {
   setPdfData(props)
 }
 
-const sendForm = () =>{
-  axios.post(`${process.env.NEXT_PUBLIC_NODE_SERVER}/formSign`, {pdfData,Uid,FolderContent}, {
-    headers:{
-        Authorization: token,
-        'Content-Type': 'multipart/form-data',
-    }
+const {getRootProps, getInputProps, isDragActive} = useDropzone({
+  onDrop: acceptedFiles => {
+      setFiles(acceptedFiles)
+  },
+  multiple: true,
+  accept: 'image/*, .pdf',
 })
+
+const checker = async (file) => {
+
+if(pdfData.sign1 == null || pdfData.sign2 == null){alert("missing signature"); return false}
+if(file > 3) return true 
+alert("you need to fill the Installateur section with min. 3 images!")
+return false 
+
+}
+
+const sendForm = async () =>{
+
+ const info = await checker(files.length)
+
+ if(info === false) return
+
+
+
+const formData = new FormData(); // create a new FormData instance
+        
+      
+        //looping trough multiple files
+        for (let i = 0; i < files.length; i++) {
+            formData.append(`file${i}`, files[i]);
+        }
+        const pdfDataString = JSON.stringify(pdfData);
+        formData.append("pdfData", pdfDataString)
+        formData.append("Uid", Uid)
+        formData.append("FolderContent", FolderContent)
+        formData.append("textArea", textArea)
+
+        console.log(formData)
+        
+        
+axios.post(`${process.env.NEXT_PUBLIC_NODE_SERVER}/formSign`, formData,{
+    headers: {
+        Authorization: token,
+        'Content-Type': 'multipart/form-data'
+    },
+   }
+)
 .then(res=> {
   console.log(res)
     if(res.status == 200){
@@ -50,8 +96,24 @@ const sendForm = () =>{
     </FormInstallLK>
     <Spacer y={5} />
     <Container style={{color: theme.colors.primary.value, margin: 'auto',}} gap={3}>
-      
-    {/* <Textarea width ="100%" label="Notes" placeholder="Add Notes" /> */}
+
+    <details open style={{color: "white", borderRadius: "10px", padding:"15px", }}> <summary>INSTALLATEUR SECTION</summary>
+    <Spacer y={1} />
+    <div {...getRootProps()} style={{padding: '20px', border: '2px dashed #ccc', borderRadius: '5px', display: 'flex', alignItems: 'center', justifyContent: 'center', cursor: 'pointer', margin: '20px 0'}}>
+                <input {...getInputProps()} />
+                {isDragActive ? 
+                    <p>Drop the files here ...</p> :
+                    <p>Click anc select 3 images</p>
+                }
+            </div>
+
+            {files && files.map(file => (
+                <p key={file.name}>{file.name}</p>
+            ))}
+    <Spacer y={.5} />
+    <Textarea width ="100%" label="Notes" placeholder="Add Notes" value={textArea} onChange={e=> setTextArea(e.target.value)}/>
+    </details>
+    <Spacer y={5} />
     <Button onPress={sendForm}>Send</Button>
           </Container>
     </Layout>
