5a26a9ae22d1bcd4cba944d9c0dd00402b47472e
Wesley G. Obi <42078311+W3sley0Bi@users.noreply.github.com>
2023-04-04 17:54:55 +0200

    WIP

--- pages/userFolder/[Uid]/[FolderContent]/Form.js
+++ pages/userFolder/[Uid]/[FolderContent]/Form.js
@@ -19,41 +19,74 @@ export default function Form() {
   const { theme } = useTheme();
   const { Uid, FolderContent } = router.query;
   const [pdfData, setPdfData] = useState();
-
+  const [textArea, setTextArea] = useState();
   const [isDisabled, setIsDisabled] = useState(false);
-
+  const [fileList, setFileList] = useState(null);
+  // const [images, setImages] = useState()
   const token = useSelector((state) => state.token.value);
   const uid = useSelector((state) => state.uid.value);
   const role = useSelector((state) => state.role.value);
 
+  const files = fileList ? [...fileList] : [];
 
   const callback = (props) => {
     setPdfData(props);
   };
 
-  const checker = async () => {
+  const checker = async (file) => {
     if (pdfData.sign1 == null || pdfData.sign2 == null) {
       alert("missing signature");
       return false;
     }
+
+    if (files.length >= 1) return true;
+    alert("you need to fill the Installateur section with min. 3 images!");
+    return false;
   };
 
   const sendForm = async () => {
-    const info = await checker();
+    const info = await checker(files.length);
 
     if (info === false) return;
 
     const formData = new FormData(); // create a new FormData instance
+    console.log(files);
+//    looping trough multiple files
+    if (files.length > 0) {
+      files.forEach((file, i) => {
+        if (file instanceof File) {
+          formData.append(`file-${i}`, file, file.name);
+        }
+      });
+    }
+
+    // for (let i = 0; i < files.length; i++) {
+    //   const reader = new FileReader();
+    //   reader.readAsArrayBuffer(files[i]);
+    //   reader.onloadend = async () => {
+    //     const base64data = btoa(
+    //       new Uint8Array(reader.result)
+    //         .reduce((data, byte) => data + String.fromCharCode(byte), '')
+    //     );
+    //     await setImages((prevState) => ({
+    //       ...prevState,
+    //       [`file${i}`]: base64data,
+    //     }));
+    //   };
+    // }
+    // const imagesString = JSON.stringify(images);
+    
+    // console.log(images)
 
     const pdfDataString = JSON.stringify(pdfData);
     
-
+   
     formData.append("pdfData", pdfDataString);
     formData.append("Uid", Uid);
     formData.append("FolderContent", FolderContent);
+    formData.append("textArea", textArea);
 
-
-    setIsDisabled(true);
+    //setIsDisabled(true);
     axios
       .post(`${process.env.NEXT_PUBLIC_NODE_SERVER}/formSign`, formData, {
         headers: {
@@ -65,7 +98,7 @@ export default function Form() {
         console.log(res);
         if (res.status == 200) {
           alert("form sent correctly");
-          router.push(`/`)
+         // router.push(`/`)
         } else {
           alert(res.status);
           console.log(res);
@@ -83,7 +116,34 @@ export default function Form() {
           style={{ color: theme.colors.primary.value, margin: "auto" }}
           gap={3}
         >
-          
+          <details
+            open
+            style={{ color: "white", borderRadius: "10px", padding: "15px" }}
+          >
+            {" "}
+            <summary>INSTALLATEUR SECTION</summary>
+            <Spacer y={1} />
+            <input
+              type="file"
+              onChange={(e) => setFileList(e.target.files)}
+              multiple
+            />
+            <ul>
+              {files.map((file, i) => (
+                <li key={i}>
+                  {file.name} - {file.type}
+                </li>
+              ))}
+            </ul>
+            <Spacer y={0.5} />
+            <Textarea
+              width="90%"
+              label="Notes"
+              placeholder="Add Notes"
+              value={textArea}
+              onChange={(e) => setTextArea(e.target.value)}
+            />
+          </details>
           <Spacer y={5} />
           <Button onPress={sendForm} disabled={isDisabled}>
             Send
--- pages/userFolder/[Uid]/[FolderContent]/PicturesNotes.js
+++ pages/userFolder/[Uid]/[FolderContent]/PicturesNotes.js
@@ -1,135 +1,135 @@
-import {
-    useTheme,
-    NextUIProvider,
-    Text,
-    Textarea,
-    Button,
-    Spacer,
-    Container,
-  } from "@nextui-org/react";
-  import Layout from "../../../../components/Layout";
-  import FormInstallLK from "../../../../components/FormInstallLK";
-  import { useState, useEffect, useMemo } from "react";
-  import { useSelector, useDispatch } from "react-redux";
-  import { useRouter } from "next/router";
-  import axios from "axios";
+// import {
+//     useTheme,
+//     NextUIProvider,
+//     Text,
+//     Textarea,
+//     Button,
+//     Spacer,
+//     Container,
+//   } from "@nextui-org/react";
+//   import Layout from "../../../../components/Layout";
+//   import FormInstallLK from "../../../../components/FormInstallLK";
+//   import { useState, useEffect, useMemo } from "react";
+//   import { useSelector, useDispatch } from "react-redux";
+//   import { useRouter } from "next/router";
+//   import axios from "axios";
   
-  export default function Form() {
-    const router = useRouter();
-    const { theme } = useTheme();
-    const { Uid, FolderContent } = router.query;
-    const [textArea, setTextArea] = useState();
-    const [isDisabled, setIsDisabled] = useState(false);
-    const [fileList, setFileList] = useState(null);
-    // const [images, setImages] = useState()
-    const token = useSelector((state) => state.token.value);
-    const uid = useSelector((state) => state.uid.value);
-    const role = useSelector((state) => state.role.value);
+//   export default function Form() {
+//     const router = useRouter();
+//     const { theme } = useTheme();
+//     const { Uid, FolderContent } = router.query;
+//     const [textArea, setTextArea] = useState();
+//     const [isDisabled, setIsDisabled] = useState(false);
+//     const [fileList, setFileList] = useState(null);
+//     // const [images, setImages] = useState()
+//     const token = useSelector((state) => state.token.value);
+//     const uid = useSelector((state) => state.uid.value);
+//     const role = useSelector((state) => state.role.value);
   
-    const files = fileList ? [...fileList] : [];
+//     const files = fileList ? [...fileList] : [];
   
   
-    const checker = async (file) => {
+//     const checker = async (file) => {
   
-      if (file >= 1) return true;
-      alert("you need to fill the Installateur section with min. 3 images!");
-      return false;
-    };
+//       if (file >= 1) return true;
+//       alert("you need to fill the Installateur section with min. 3 images!");
+//       return false;
+//     };
   
-    const sendForm = async () => {
-      const info = await checker(files.length);
+//     const sendForm = async () => {
+//       const info = await checker(files.length);
   
-      if (info === false) return;
+//       if (info === false) return;
   
-      const formData = new FormData(); // create a new FormData instance
+//       const formData = new FormData(); // create a new FormData instance
 
-  //    looping trough multiple files
-  setIsDisabled(true);
-  formData.append("folder", FolderContent);
+//   //    looping trough multiple files
+//   setIsDisabled(true);
+//   formData.append("folder", FolderContent);
 
-      if (files.length > 0) {
+//       if (files.length > 0) {
 
-        files.forEach((file, i) => {
-          if (file instanceof File) {
-            formData.append(`file-${i}`, file, file.name);
+//         // files.forEach((file, i) => {
+//         //   if (file instanceof File) {
+//         //     formData.append(`file-${i}`, file, file.name);
 
 
-            axios
-            .post(`${process.env.NEXT_PUBLIC_NODE_SERVER}/imageUpload`, formData, {
-              headers: {
-                Authorization: token,
-                "Content-Type": "multipart/form-data",
-              },
-            })
-            .then((res) => {
-              console.log(res);
-              if (res.status == 200) {
-                //alert(`data sent correctly. status ${res.status}`);
-                //router.push(`/userFolder/${Uid}/${FolderContent}/Form`)
-              } else {
-                alert(res.status);
-                console.log(res);
-              }
-            })
-            .catch((err) => console.log(err));
+//         //     axios
+//         //     .post(`${process.env.NEXT_PUBLIC_NODE_SERVER}/imageUpload`, formData, {
+//         //       headers: {
+//         //         Authorization: token,
+//         //         "Content-Type": "multipart/form-data",
+//         //       },
+//         //     })
+//         //     .then((res) => {
+//         //       console.log(res);
+//         //       if (res.status == 200) {
+//         //         //alert(`data sent correctly. status ${res.status}`);
+//         //         //router.push(`/userFolder/${Uid}/${FolderContent}/Form`)
+//         //       } else {
+//         //         alert(res.status);
+//         //         console.log(res);
+//         //       }
+//         //     })
+//         //     .catch((err) => console.log(err));
 
-            formData.delete(`file-${i}`);
-          }
-        });
-      }
+//         //     formData.delete(`file-${i}`);
+//         //   }
+//         // });
+//       }
 
-       //formData.append("textArea", textArea);
+//        //formData.append("textArea", textArea);
     
       
 
     
 
-    };
+//     };
   
-    return (
-      <>
-        <Layout>
-          <Spacer y={5} />
-          <Container
-            style={{ color: theme.colors.primary.value, margin: "auto" }}
-            gap={3}
-          >
-            <details
-              open
-              style={{ color: "white", borderRadius: "10px", padding: "15px" }}
-            >
-              {" "}
-              <summary>INSTALLATEUR SECTION</summary>
-              <Spacer y={1} />
-              <input
-                type="file"
-                onChange={(e) => setFileList(e.target.files)}
-                multiple
-              />
-              <ul>
-                {files.map((file, i) => (
-                  <li key={i}>
-                    {file.name} - {file.type}
-                  </li>
-                ))}
-              </ul>
-              <Spacer y={0.5} />
-              <Textarea
-                width="100%"
-                label="Notes"
-                placeholder="Add Notes"
-                value={textArea}
-                onChange={(e) => setTextArea(e.target.value)}
-              />
-            </details>
-            <Spacer y={5} />
-            <Button onPress={sendForm} disabled={isDisabled}>
-              Next
-            </Button>
-            <Spacer y={5} />
-          </Container>
-        </Layout>
-      </>
-    );
-  }
+//     return (
+//       <>
+//         <Layout>
+//           <Spacer y={5} />
+//           <Container
+//             style={{ color: theme.colors.primary.value, margin: "auto" }}
+//             gap={3}
+//           >
+//             <details
+//               open
+//               style={{ color: "white", borderRadius: "10px", padding: "15px" }}
+//             >
+//               {" "}
+//               <summary>INSTALLATEUR SECTION</summary>
+//               <Spacer y={1} />
+//               <input
+//                 type="file"
+//                 onChange={(e) => setFileList(e.target.files)}
+//                 multiple
+//               />
+//               <ul>
+//                 {files.map((file, i) => (
+//                   <li key={i}>
+//                     {file.name} - {file.type}
+//                   </li>
+//                 ))}
+//               </ul>
+//               <Spacer y={0.5} />
+//               <Textarea
+//                 width="100%"
+//                 label="Notes"
+//                 placeholder="Add Notes"
+//                 value={textArea}
+//                 onChange={(e) => setTextArea(e.target.value)}
+//               />
+//             </details>
+//             <Spacer y={5} />
+//             <Button onPress={sendForm} disabled={isDisabled}>
+//               Next
+//             </Button>
+//             <Spacer y={5} />
+//           </Container>
+//         </Layout>
+//       </>
+//     );
+//   }
   
